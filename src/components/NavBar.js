1337791ef62011983e47096e88a7f0319307753b
David <dmchoull@gmail.com>
2018-06-24 20:55:41 -0400

    feat: add basic navbar, show daily deals on home page

---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const NavBar = () => (
+  <nav>
+    <span>Web Store</span>
+
+    <style jsx>{`
+      nav {
+        background-color: #d3d3d3;
+        height: 50px;
+        font-size: 24px;
+        line-height: 50px;
+        padding: 0 50px;
+      }
+    `}</style>
+  </nav>
+);
+
+export default NavBar;
